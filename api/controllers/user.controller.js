b24ab6320cbbe37976149db2e80f3122f8a816a7
gayathri140205 <gayathri140205@gmail>
2024-06-23 10:51:19 +0530

    Apii

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import bcryptjs from 'bcryptjs';
-import {errorHandler} from '../utils/error.js';
+import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
 import Listing from '../models/listing.model.js';
 
@@ -10,61 +10,137 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id) return next(errorHandler(401, "you can only upate your own account"));
     try {
         if(req.body.password) {
-         req.body.password = bcryptjs.hashSync(req.body.password, 10); 
+            req.body.password = bcryptjs.hashSync(req.body.password, 10); 
         }
 
         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
             $set: {
-              username: req.body.username,
-              email: req.body.email,
-              password: req.body.password,
-              avatar: req.body.avatar,
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
             }
-        }, { new: true})
+        }, { new: true });
 
-        const { password, ...rest } = updatedUser._doc
-        
-        res.status(200).json(rest)
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
     } catch (error) {
-        next(error)
+        next(error);
     }
 };
 
-
 export const deleteUser = async (req, res, next) => {
-   if(req.user.id !== req.params.id) return next(errorHandler(401, 'you can delete only your own account')); 
     try {
         await User.findByIdAndDelete(req.params.id);
         res.clearCookie('access_token');
         res.status(200).json('User has been deleted!');
     } catch (error) {
-       next(error) 
+        next(error);
     }
 };
 
-export const getUserListings= async (req, res, next) => {
-  if (req.user.id === req.params.id) {
-     try {
-        const listings = await Listing.find({ userRef: req.params.id});
+export const getUserListings = async (req, res, next) => {
+    try {
+        const listings = await Listing.find({ userRef: req.params.id });
         res.status(200).json(listings);
-     } catch (error) {
-       next(error);        
-     }
-  } else {
-    return next(errorHandler(401, 'You can only view your own listings'));
-  }
+    } catch (error) {
+        next(error);
+    }
 };
 
-export const getUser = async(req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) return next(errorHandler(404, 'User not found'));
-    const { password: pass, ...rest} = user._doc;
-    res.status(200).json(rest);
-  } catch (error) {
-    next(error);  
-  }
-}
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return next(errorHandler(404, 'User not found'));
+        const { password: pass, ...rest } = user._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import bcryptjs from 'bcryptjs';
+// import {errorHandler} from '../utils/error.js';
+// import User from '../models/user.model.js';
+// import Listing from '../models/listing.model.js';
+
+// export const test = (req, res) => {
+//     res.json({ 
+//         message: "Api route is working"
+//     });
+// };
+
+// export const updateUser = async (req, res, next) => {
+//     if (req.user.id !== req.params.id) return next(errorHandler(401, "you can only upate your own account"));
+//     try {
+//         if(req.body.password) {
+//          req.body.password = bcryptjs.hashSync(req.body.password, 10); 
+//         }
+
+//         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+//             $set: {
+//               username: req.body.username,
+//               email: req.body.email,
+//               password: req.body.password,
+//               avatar: req.body.avatar,
+//             }
+//         }, { new: true})
+
+//         const { password, ...rest } = updatedUser._doc
+        
+//         res.status(200).json(rest)
+//     } catch (error) {
+//         next(error)
+//     }
+// };
+
+
+// export const deleteUser = async (req, res, next) => {
+//    if(req.user.id !== req.params.id) return next(errorHandler(401, 'you can delete only your own account')); 
+//     try {
+//         await User.findByIdAndDelete(req.params.id);
+//         res.clearCookie('access_token');
+//         res.status(200).json('User has been deleted!');
+//     } catch (error) {
+//        next(error) 
+//     }
+// };
+
+// export const getUserListings= async (req, res, next) => {
+//   if (req.user.id === req.params.id) {
+//      try {
+//         const listings = await Listing.find({ userRef: req.params.id});
+//         res.status(200).json(listings);
+//      } catch (error) {
+//        next(error);        
+//      }
+//   } else {
+//     return next(errorHandler(401, 'You can only view your own listings'));
+//   }
+// };
+
+// export const getUser = async(req, res, next) => {
+//   try {
+//     const user = await User.findById(req.params.id);
+//     if (!user) return next(errorHandler(404, 'User not found'));
+//     const { password: pass, ...rest} = user._doc;
+//     res.status(200).json(rest);
+//   } catch (error) {
+//     next(error);  
+//   }
+// }
