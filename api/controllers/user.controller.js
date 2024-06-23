9b3742818e297dd7e05219cda50ad88ee5d171d4
gayathri140205 <gayathri140205@gmail>
2024-06-23 11:41:57 +0530

    api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,28 +8,32 @@ export const test = (req, res) => {
         message: "Api route is working"
     });
 };
-
 export const updateUser = async (req, res, next) => {
-    try {
-        if(req.body.password) {
-            req.body.password = bcryptjs.hashSync(req.body.password, 10); 
-        }
+  try {
+      if(req.body.password) {
+          req.body.password = bcryptjs.hashSync(req.body.password, 10); 
+      }
 
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-            $set: {
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: req.body.avatar,
-            }
-        }, { new: true });
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+          $set: {
+              username: req.body.username,
+              email: req.body.email,
+              password: req.body.password,
+              avatar: req.body.avatar,
+          }
+      }, { new: true });
 
-        const { password, ...rest } = updatedUser._doc;
-        res.status(200).json(rest);
-    } catch (error) {
-        next(error);
-    }
+      if (!updatedUser) {
+          return next(errorHandler(404, 'User not found'));
+      }
+
+      const { password, ...rest } = updatedUser._doc;
+      res.status(200).json(rest);
+  } catch (error) {
+      next(error);
+  }
 };
+
 
 export const deleteUser = async (req, res, next) => {
     try {
