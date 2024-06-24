fe8898f8c982c120ca638545a7831876d0a88ee1
gayathri140205 <gayathri140205@gmail>
2024-06-24 08:45:07 +0530

    api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing, 
-    getListing, getListings } from '../controllers/listing.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
-router.post('/create', verifyToken, createListing);
+// Removed the verifyToken middleware from routes
+router.post('/create', createListing);
 router.delete('/delete/:id', deleteListing);
-router.post('/update/:id', verifyToken, updateListing);
+router.post('/update/:id', updateListing);
 router.get('/get/:id', getListing);
 router.get('/get', getListings);
 
